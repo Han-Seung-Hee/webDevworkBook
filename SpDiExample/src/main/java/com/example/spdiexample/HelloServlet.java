97997f5099c5039ef5846c5a290bd19981e5b9f4
Han-Seung-Hee <xorhd1222@gmail.com>
2022-12-06 21:48:04 +0900

    2022-12-07 JakartaEE -> JavaEE

--- SpDiExample/src/main/java/com/example/spdiexample/HelloServlet.java
+++ SpDiExample/src/main/java/com/example/spdiexample/HelloServlet.java
@@ -2,8 +2,13 @@ package com.example.spdiexample;
 
 import java.io.*;
 
-import jakarta.servlet.http.*;
-import jakarta.servlet.annotation.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
