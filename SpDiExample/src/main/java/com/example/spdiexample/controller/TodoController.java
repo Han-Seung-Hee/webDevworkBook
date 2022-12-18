c7ba26cc74a48a96139c09ef2e3873cb0de8c40e
Han-Seung-Hee <xorhd1222@gmail.com>
2022-12-18 20:38:11 +0900

    20221218

--- SpDiExample/src/main/java/com/example/spdiexample/controller/TodoController.java
+++ SpDiExample/src/main/java/com/example/spdiexample/controller/TodoController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Log4j2
 @Controller
@@ -34,5 +35,20 @@ public class TodoController {
     public void ex4(Model model){
         log.info("-----------------------------------");
         model.addAttribute("message","hello, world");
+    }
+
+    //Redirect
+    @PostMapping("/ex5")
+    public String ex5(RedirectAttributes redirectAttributes)
+    {
+        redirectAttributes.addAttribute("name","ABC"); // 쿼리 스트링에 추가
+        redirectAttributes.addFlashAttribute("result","success"); // JSP에서 일회용으로 사용가능
+
+        return "redirect:/ex6";
+    }
+
+    @GetMapping("/ex6")
+    public void ex6(){
+        
     }
 }
